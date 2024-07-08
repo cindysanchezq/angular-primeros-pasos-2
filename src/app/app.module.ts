c73c36f6eca4e28022a272364ebd3836027f685a
CindySanchez Quintana <cindysanchez28@outlook.com>
2024-07-08 17:12:15 -0500

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { CounterModule } from './counter/counter.module';
+import { HeroesModule } from './heroes/heroes.module';
+import { CommonModule } from '@angular/common';
+import { DbzModule } from './dbz/dbz.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CounterModule,
+    HeroesModule,
+    CommonModule, 
+    DbzModule
   ],
   providers: [],
   bootstrap: [AppComponent]
